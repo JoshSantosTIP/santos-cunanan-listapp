7b83bc18110c96e7c5ad8fe96fe5b9dce14709ab
JoshSantos <qjmsantos04@tip.edu.ph>
2025-10-03 09:22:01 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SANTOS_CUNANAN_LISTAPP',
+  webDir: 'www'
+};
+
+export default config;
